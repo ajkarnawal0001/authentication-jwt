fc545fdfd2cb6210bee0978bd3caecc1fc030536
Amarjeet <amarjeet.s@solutelabs.com>
2022-09-23 00:39:10 +0530

    all code

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+  return (
+    <BrowserRouter>
+      <Container maxWidth="xl">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={() => <Redirect to="/data" />} />
+          <Route path="/users" exact component={Home} />
+          <Route path="/users/search" exact component={Home} />
+          <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Container, Grow } from '@material-ui/core';
+
+const Home = () => {
+  
+  return (
+    <Grow in>
+      <Container maxWidth="xl">
+        <h1>hello users</h1>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
